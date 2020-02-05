2a18c0e96335ae5cc6b67fb37004b41db1c62a2a
Ricardo <passocurto@gmail.com>
2020-02-05 10:36:41 -0300

    subir tudo

---
+++ build/precache-manifest.18ac6aa97492bc699fe7bcade6a5b147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7678ff4780627482bc8a689df81684",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f3036db1c4663046355",
+    "url": "/static/css/main.44f640b9.chunk.css"
+  },
+  {
+    "revision": "ad2ffa42296c69de1b0f",
+    "url": "/static/js/2.a4764e73.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a4764e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3036db1c4663046355",
+    "url": "/static/js/main.39e08d77.chunk.js"
+  },
+  {
+    "revision": "ccd1adbe065bbf296b21",
+    "url": "/static/js/runtime-main.7c7f9f47.js"
+  }
+]);
